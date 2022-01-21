9f5bb50746c1c0d15682ab922542e834b58f31ab
Admin <admin@Admins-MacBook-Pro.local>
2022-01-21 14:21:48 -0500

    conditionally render student name and interviewer

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { Fragment } from "react";
 import "components/Appointment/styles.scss";
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
 
 export default function Appointment(props) {
-  const { time } = props;
-  const appointmentText = !time ? "No Appointments" : `Appointment at ${time}`;
-  return <article className="appointment">{appointmentText}</article>;
+  const { time, interview } = props;
+  // const appointmentText = !time ? "No Appointments" : `Appointment at ${time}`;
+  return (
+    <Fragment>
+      <Header time={time} />
+      <article className="appointment">
+        {interview ? <Show interview={interview} time={time} /> : <Empty />}
+      </article>
+    </Fragment>
+  );
 }
